c282c95068c3b27a2571347ace60350db5909201
Farrawy <ahmed.e.alfarra@gmail.com>
2022-01-29 00:05:51 +0300

    Authentication Activities Updated

--- app/src/main/java/com/example/foody/ui/ActivitiesPackage/Register/RegisterActivity.kt
+++ app/src/main/java/com/example/foody/ui/ActivitiesPackage/Register/RegisterActivity.kt
@@ -4,8 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import com.example.foody.R
+import com.example.foody.ui.ActivitiesPackage.Login.LoginActivity
 import com.example.foody.ui.MainActivity
+import org.w3c.dom.Text
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,9 +17,16 @@ class RegisterActivity : AppCompatActivity() {
         setContentView(R.layout.activity_register)
 
         val sign_up_btn = findViewById<Button>(R.id.sign_up_btn)
+        val register_login_textView = findViewById<TextView>(R.id.register_login_textView)
 
         sign_up_btn.setOnClickListener{
             val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        register_login_textView.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
         }
--- app/src/main/java/com/example/foody/ui/ActivitiesPackage/Welcome/WelcomeActivity.kt
+++ app/src/main/java/com/example/foody/ui/ActivitiesPackage/Welcome/WelcomeActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import com.example.foody.R
 import com.example.foody.ui.ActivitiesPackage.Login.LoginActivity
 import com.example.foody.ui.ActivitiesPackage.Register.RegisterActivity
@@ -17,15 +18,20 @@ class WelcomeActivity : AppCompatActivity() {
 
         val welcome_login_btn = findViewById<Button>(R.id.welcome_login_btn)
         val welcome_sign_up_btn = findViewById<Button>(R.id.welcome_sign_up_btn)
+        val welcome_signing_up = findViewById<TextView>(R.id.welcome_signing_up)
 
         welcome_login_btn.setOnClickListener{
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
-            finish()
         }
 
         welcome_sign_up_btn.setOnClickListener{
             val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        welcome_signing_up.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
         }
